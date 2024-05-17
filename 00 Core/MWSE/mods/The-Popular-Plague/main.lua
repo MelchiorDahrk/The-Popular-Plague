390d6118a0193a93c375f051b7993a32a945d4f1
Greatness7 <Greatness7@gmail.com>
2024-05-17 19:33:48 -0400

    fix mwse-lua animation interrupt logic

--- 00 Core/MWSE/mods/The-Popular-Plague/main.lua
+++ 00 Core/MWSE/mods/The-Popular-Plague/main.lua
@@ -12,11 +12,14 @@ local function onReferenceActivated(e)
         if actor.position:distance(position) <= 8
             and actor.object.race.isBeast == false
         then
+            -- Play the dancing animation
             tes3.playAnimation({
                 reference = actor.reference,
                 mesh = "md24\\r\\belly_dance.nif",
                 group = tes3.animationGroup.idle9,
             })
+            -- Set dancing flag
+            actor.reference.data.md24_dancing = true
         end
     end
 end
@@ -26,28 +29,26 @@ event.register("referenceActivated", onReferenceActivated)
 ---
 ---@param e combatStartedEventData
 local function onCombatStarted(e)
-    local object = e.target.object
+    local ref = e.actor.reference
 
-    if object.objectType ~= tes3.objectType.npc then
+    -- Is dancing flag was set
+    if not ref.data.md24_dancing then
         return
     end
 
-    if object.mesh:lower() ~= "md24\\r\\belly_dance.nif" then
-        return
-    end
+    -- Clear the dancing flag
+    ref.data.md24_dancing = nil
 
-    local currentGroup = tes3.getAnimationGroups({ reference = e.target.reference })
-    if currentGroup == tes3.animationGroup.idle9 then
-        tes3.playAnimation({
-            reference = e.target.reference,
-            group = table.choice({
-                tes3.animationGroup.hit1,
-                tes3.animationGroup.hit2,
-                tes3.animationGroup.hit3,
-                tes3.animationGroup.hit4,
-                tes3.animationGroup.hit5,
-            }),
-        })
-    end
+    -- Interrupt the animation
+    tes3.playAnimation({
+        reference = e.actor.reference,
+        group = table.choice({
+            tes3.animationGroup.hit1,
+            tes3.animationGroup.hit2,
+            tes3.animationGroup.hit3,
+            tes3.animationGroup.hit4,
+            tes3.animationGroup.hit5,
+        }),
+    })
 end
 event.register("combatStarted", onCombatStarted)

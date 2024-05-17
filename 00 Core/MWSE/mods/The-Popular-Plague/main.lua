a08929a2a5ec96a86e948a9fadbfd96ea40a77ff
Greatness7 <Greatness7@gmail.com>
2024-05-17 19:15:40 -0400

    interrupt dancing on combat start

--- 00 Core/MWSE/mods/The-Popular-Plague/main.lua
+++ 00 Core/MWSE/mods/The-Popular-Plague/main.lua
@@ -1,15 +1,7 @@
----@param ref tes3reference
-local function startDancing(ref)
-    if ref.object.race.isBeast then return end
-
-    tes3.playAnimation({
-        reference = ref,
-        mesh = "md24\\r\\belly_dance.nif",
-        group = tes3.animationGroup.idle9,
-    })
-end
-
-event.register("referenceActivated", function(e)
+--- Play dancing animations when "md24_anim_dance" is placed.
+---
+---@param e referenceActivatedEventData
+local function onReferenceActivated(e)
     if e.reference.id ~= "md24_anim_dance" then
         return
     end
@@ -17,8 +9,45 @@ event.register("referenceActivated", function(e)
     local position = e.reference.position
     local actors = tes3.findActorsInProximity({ position = position, range = 128 })
     for _, actor in pairs(actors) do
-        if actor.position:distance(position) <= 8 then
-            startDancing(actor.reference)
+        if actor.position:distance(position) <= 8
+            and actor.object.race.isBeast == false
+        then
+            tes3.playAnimation({
+                reference = actor.reference,
+                mesh = "md24\\r\\belly_dance.nif",
+                group = tes3.animationGroup.idle9,
+            })
         end
     end
-end)
+end
+event.register("referenceActivated", onReferenceActivated)
+
+--- Cancel dancing animations when combat is started.
+---
+---@param e combatStartedEventData
+local function onCombatStarted(e)
+    local object = e.target.object
+
+    if object.objectType ~= tes3.objectType.npc then
+        return
+    end
+
+    if object.mesh:lower() ~= "md24\\r\\belly_dance.nif" then
+        return
+    end
+
+    local currentGroup = tes3.getAnimationGroups({ reference = e.target.reference })
+    if currentGroup == tes3.animationGroup.idle9 then
+        tes3.playAnimation({
+            reference = e.target.reference,
+            group = table.choice({
+                tes3.animationGroup.hit1,
+                tes3.animationGroup.hit2,
+                tes3.animationGroup.hit3,
+                tes3.animationGroup.hit4,
+                tes3.animationGroup.hit5,
+            }),
+        })
+    end
+end
+event.register("combatStarted", onCombatStarted)
--- 00 Core/scripts/The-Popular-Plague/npc.lua
+++ 00 Core/scripts/The-Popular-Plague/npc.lua
@@ -1,17 +1,32 @@
+local self = require("openmw.self")
+local types = require("openmw.types")
+local animation = require("openmw.animation")
+local interfaces = require('openmw.interfaces')
+
 return {
+    engineHandlers = {
+        --- Cancel dancing animations when combat is started.
+        ---
+        onUpdate = function()
+            local activePackage = interfaces.AI.getActivePackage()
+            if activePackage ~= nil
+                and activePackage.target ~= nil
+                and animation.isPlaying(self, "bellydance")
+            then
+                animation.cancel(self, "bellydance")
+            end
+        end
+    },
     eventHandlers = {
+        --- Play dancing animations when "md24_anim_dance" is placed.
+        ---
         md24_anim_dance = function(data)
-            local self = require("openmw.self")
-            local types = require("openmw.types")
-
-
             local npc = types.NPC.records[self.recordId]
             local race = types.NPC.races.records[npc.race]
             if race.isBeast then
                 return
             end
 
-            local animation = require("openmw.animation")
             animation.playBlended(self, "bellydance", { priority = animation.PRIORITY.Scripted })
         end,
     }

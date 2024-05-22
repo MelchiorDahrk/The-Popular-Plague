fe819e089b145a1cbfaaf8a97d37ab3cbb9926bd
Greatness7 <Greatness7@gmail.com>
2024-05-22 19:13:54 -0400

    Implement music systems. Refactor code file structures.

---
+++ 00 Core/MWSE/mods/The-Popular-Plague/music.lua
@@ -0,0 +1,34 @@
+---@parma cell tes3cell?
+local function isEidolonCell(cell)
+    return (cell ~= nil) and (cell.name:find("Eidolon") ~= nil)
+end
+
+--- When first entering an Eidolon, trigger our custom music.
+---
+---@param e cellChangedEventData
+local function onCellChanged(e)
+    local isEidolon = isEidolonCell(e.cell)
+    local wasEidolon = isEidolonCell(e.previousCell)
+    if isEidolon and not wasEidolon then
+        tes3.streamMusic({
+            path = "md24/The_Golden_Tower.mp3",
+            situation = tes3.musicSituation.explore,
+        })
+    end
+end
+event.register("cellChanged", onCellChanged, { priority = 720 })
+
+
+--- Override new music track selections while in an Eidolon cell.
+---
+--- @param e musicSelectTrackEventData
+local function onMusicSelectTrack(e)
+    if e.situation == tes3.musicSituation.combat then
+        return -- allow combat music to play as normal
+    end
+    if cell.name = "The Golden Tower" then
+        e.music = "The_Golden_Tower.mp3"
+        e.situation = tes3.musicSituation.explore
+    end
+end
+event.register("musicSelectTrack", onMusicSelectTrack, { priority = 720 })
